e09c0a512669e487f99b931a210326f33bf42a2e
Orim Dominic <orimdominic@gmail.com>
2021-05-03 23:28:58 +0100

    refactor: add register-webhook executable

--- api/twitter-webhook-listener.ts
+++ api/twitter-webhook-listener.ts
@@ -6,21 +6,18 @@ module.exports = async (req: VercelRequest, res: VercelResponse) => {
   const method = req.method.toLowerCase();
   switch (method) {
     case "get": {
-      console.log(req)
       const { crc_token } = req.query;
-      console.log("token: ---------", crc_token)
       if (typeof crc_token === "string" && crc_token.length) {
-        res.status(200).json({
+        return res.status(200).json({
           response_token: getChallengeResponse(crc_token),
         });
       }
-      res
+      return res
         .status(StatusCodes.BAD_REQUEST)
         .send(getReasonPhrase(StatusCodes.BAD_REQUEST));
-      return;
     }
     default:
-      res
+      return res
         .status(StatusCodes.METHOD_NOT_ALLOWED)
         .send(getReasonPhrase(StatusCodes.METHOD_NOT_ALLOWED));
       break;
---
+++ src/bin/register-webhook.ts
@@ -0,0 +1,41 @@
+import * as readline from "readline";
+const rl = readline.createInterface(process.stdin, process.stdout);
+import * as util from "util";
+import * as request from "request";
+require("../utils/config");
+
+const post = util.promisify(request.post);
+
+const twitterOAuth = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY as string,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET as string,
+  token: process.env.TWITTER_ACCESS_TOKEN as string,
+  token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET as string,
+};
+
+rl.question(
+  "Your Twitter development environment label: ",
+  function (envLabel: string) {
+    rl.question("Your webhook URL: ", function (webhookUrl: string) {
+      const requestOptions = {
+        url: `https://api.twitter.com/1.1/account_activity/all/${envLabel}/webhooks.json`,
+        oauth: twitterOAuth,
+        headers: {
+          "Content-type": "application/x-www-form-urlencoded",
+        },
+        form: {
+          url: webhookUrl,
+        },
+      };
+      (async (opts) => {
+        try {
+          const body = await post(opts);
+          console.log("result", JSON.stringify(body));
+        } catch (e) {
+          console.error("error: ", JSON.stringify(e));
+        }
+      })(requestOptions);
+      rl.close();
+    });
+  }
+);
