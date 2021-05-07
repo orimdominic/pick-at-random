c1d3d272b2740e3cd28f16e973b0571fa8c51914
Orim Dominic Adah <orimdominic@gmail.com>
2021-05-07 19:40:52 +0100

    refactor: set real mention (#16)
    * refactor handlePickAtRandomActivity
    
    * ci: set GitHub acrions to run on only push and PR to main branch
    
    * style: prettier format
    
    * refactor: set DMEvent interface
    
    * refactor: validate received tweet for analysis
    
    * fix: add PICKATRANDOM_SCREEN_NAME env var
    
    * fix: add PICKATRANDOM_SCREEN_NAME env var
    
    * fix: add PICKATRANDOM_SCREEN_NAME env var
    
    * refactor: parse tweet object to real mention tweet type
    
    * refactor: parse command text
    
    * chore: remove unnecessary text from PR template

--- src/webhook-listener-helpers/__tests__/handle-pick-at-random-activity-create-tweet-events.spec.ts
+++ src/webhook-listener-helpers/__tests__/handle-pick-at-random-activity-create-tweet-events.spec.ts
@@ -1,7 +1,14 @@
 require("../../utils/config");
 
-import { ITweet, isRealMention } from "..";
-import { mockTweet } from "../__mocks__/test-data";
+import {
+  ITweet,
+  IRealMentionTweet,
+  isRealMention,
+  setRealMention,
+  setCommandText,
+  isValidCommandText,
+} from "..";
+import { mockRealMention, mockTweet } from "../__mocks__/test-data";
 
 describe("isRealMention", () => {
   it("returns false if a tweet is by PickAtRandom", () => {
@@ -39,5 +46,57 @@ describe("isRealMention", () => {
     } as ITweet;
     const val = isRealMention(tweetByPickAtRandom);
     expect(val).toBe(true);
+  });
+});
+
+describe("setRealMention", () => {
+  it("sets `text` to full text if `text` was truncated", () => {
+    const tweetFullText = "full text of tweet";
+    const tweet: ITweet = {
+      ...mockTweet,
+      truncated: true,
+      extended_tweet: { full_text: tweetFullText },
+    };
+    expect(setRealMention(tweet).text).toBe(tweetFullText);
+  });
+});
+
+describe("setCommandText", () => {
+  it("parses and sets the correct command text on its tweet argument", () => {
+    const mockExpectations = [
+      { text: "@PickAtRandom hello", cmdText: "hello" },
+      {
+        text: "@PickAtRandom 3 retweets in 2 days",
+        cmdText: "3 retweets in 2 days",
+      },
+      {
+        text:
+          "@PickAtRandom will do the job! @PickAtRandom 3 retweets in 2 days",
+        cmdText: "3 retweets in 2 days",
+      },
+    ];
+
+    for (const exp of mockExpectations) {
+      const tweet: IRealMentionTweet = {
+        ...mockRealMention,
+        text: exp.text,
+      };
+      expect(setCommandText(tweet).cmdText).toBe(exp.cmdText);
+    }
+  });
+});
+
+describe("isValidCommandText", () => {
+  it("returns true if the command text is valid", () => {
+    const mockExpectations = [
+      { cmdText: "hello", val: false },
+      { cmdText: "@mykeels says hi", val: false },
+      { cmdText: "4 retweets tomorrow", val: true },
+      { cmdText: "3 retweets in 2 days", val: true },
+      { cmdText: "3 retweets on thursday", val: true },
+    ];
+    for (const exp of mockExpectations) {
+      expect(isValidCommandText(exp.cmdText)).toBe(exp.val);
+    }
   });
 });
--- src/webhook-listener-helpers/handle-pick-at-random-tweet-create-events.ts
+++ src/webhook-listener-helpers/handle-pick-at-random-tweet-create-events.ts
@@ -1,4 +1,4 @@
-import { ITweet } from "./ITweet";
+import { IRealMentionTweet, ITweet } from "./ITweet";
 import { VercelResponse } from "@vercel/node";
 
 /**
@@ -12,7 +12,7 @@ import { VercelResponse } from "@vercel/node";
  * @param {ITweet} tweet - The tweet to validate
  * @returns {boolean} true if it is a real mention
  */
-export const isRealMention = function (tweet: ITweet): boolean {
+export const isRealMention = (tweet: ITweet): boolean => {
   // ignore retweets, but accept quotes
   if (tweet.retweeted_status && !tweet.is_quote_status) {
     return false;
@@ -24,10 +24,70 @@ export const isRealMention = function (tweet: ITweet): boolean {
   return true;
 };
 
+/**
+ * Create a real mention tweet object from a full valid tweet
+ *
+ * @param {ITweet} tweet - A valid tweet
+ * @returns {IRealMentionTweet} a real mention tweet
+ */
+export const setRealMention = (tweet: ITweet): IRealMentionTweet => {
+  return {
+    createdAt: tweet.created_at,
+    id: tweet.id_str,
+    refTweetId: tweet.in_reply_to_status_id_str,
+    authorName: tweet.user.screen_name,
+    authorId: tweet.user.id_str,
+    text: tweet.truncated ? tweet.extended_tweet!.full_text : tweet.text,
+    urls: tweet.entities.urls,
+  };
+};
+
+/**
+ * Parses the text of a real mention tweet to extract
+ * and set the command text of the tweet
+ *
+ * @param {IRealMentionTweet} tweet - a real mention tweet
+ * @returns {IRealMentionTweet} - a tweet containing a command text if any
+ */
+export const setCommandText = (tweet: IRealMentionTweet): IRealMentionTweet => {
+  const lastMentionIndex = tweet.text.lastIndexOf(
+    `${process.env.PICKATRANDOM_SCREEN_NAME}`
+  );
+  const cmdText = tweet.text
+    .substring(lastMentionIndex)
+    .replace(`${process.env.PICKATRANDOM_SCREEN_NAME} `, "")
+    .toLowerCase()
+    .trim();
+  return {
+    ...tweet,
+    cmdText,
+  };
+};
+
+/**
+ * If validates a command text
+ * @param {string} text - The command text
+ * @returns {boolean} true if the command text is valid
+ */
+export const isValidCommandText = (text: string): boolean => {
+  if (text.length === 0) return false;
+  const splitText = text.split(" ");
+  // if the text doesn't start with a number
+  const [count] = splitText;
+  if (!Number.isInteger(parseInt(count, 10))) return false;
+  // at minimum, the text should be like '4 retweets tomorrow'
+  if (splitText.length < 3) return false;
+  return true;
+};
+
 export async function handlePickAtRandomTweetCreateEvents(
   events: ITweet[],
   res: VercelResponse
 ): Promise<undefined> {
+  const realMentions = events.filter(isRealMention);
+  if (!realMentions.length) {
+    res.status(200).send(null);
+  }
   console.log("called");
   return;
 }
