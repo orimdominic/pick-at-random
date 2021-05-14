1089c85fa641fbf6c112a35724b4467588edc171
Orim Dominic Adah <orimdominic@gmail.com>
2021-05-14 11:55:03 +0100

    fix: setup script for codecov (#29)

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,5 @@ module.exports = {
   moduleFileExtensions: ["ts", "js"],
   testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/", "/build/"],
-  collectCoverage: true,
   coverageReporters: ["html", "lcov"],
 };
